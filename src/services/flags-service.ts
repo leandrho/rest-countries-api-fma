0ac0bf2f1915c3fef745d51b8c3f0cb9d4d257c0
Leandrho <leandro.coder01@gmail.com>
2025-01-17 17:37:38 -0300

    add fetchs to countries api

---
+++ src/services/flags-service.ts
@@ -0,0 +1,54 @@
+import axios from "axios"
+import { CountriesInfoSchema } from "../schemas/restcountries-schema";
+import { CountryInfoList, CountryInfo } from "../types";
+
+
+const baseUrl :string = "https://restcountries.com/v3.1";
+
+export const fetchAllCountries = async () : Promise<CountryInfoList> => {
+
+    try {
+        const r = await axios.get(`${baseUrl}/all`);
+        console.log('DATA: ',r)
+        const res = CountriesInfoSchema.safeParse(r.data);
+        if(!res.success)
+            throw new Error("Invalid json parse..");
+
+        return res.data;
+    } catch (error) {
+        console.log("Can't get all countries - Error: "+error);
+        throw error;
+    }
+}
+
+export const fetchCountryByName = async ( name: string) : Promise<CountryInfo> => {
+
+    try {
+        const {data} = await axios.get(`${baseUrl}/name/${name}`);
+        console.log('DATA: ', data)
+        const res = CountriesInfoSchema.safeParse(data);
+        if(!res.success)
+            throw new Error("Invalid json parse..");
+
+        return res.data[0];
+    } catch (error) {
+        console.log("Can't get country by name - Error: "+error);
+        throw error;
+    }
+}
+
+export const fetchCountriesByRegion = async ( region :string ) : Promise<CountryInfoList> => {
+
+    try {
+        const { data } = await axios.get(`${baseUrl}/region/${region}`);
+        console.log('DATA: ',data)
+        const res = CountriesInfoSchema.safeParse(data);
+        if(!res.success)
+            throw new Error("Invalid json parse..");
+
+        return res.data;
+    } catch (error) {
+        console.log("Can't get region countries - Error: "+error);
+        throw error;
+    }
+}

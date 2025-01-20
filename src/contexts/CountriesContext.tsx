783cbce1e1966908df176141a7880478b0288723
Leandrho <leandro.coder01@gmail.com>
2025-01-20 12:49:38 -0300

    add context

---
+++ src/contexts/CountriesContext.tsx
@@ -0,0 +1,23 @@
+
+import { createContext, ReactNode } from "react"
+import { CountryInfoList, Theme } from "../types";
+import { useGlobalCountriesState } from "../hooks/useGlobalCountriesState";
+
+type CountriesContextType = {
+    countries: CountryInfoList,
+    errorMsg :string,
+    toogleTheme :() => void
+}
+export const CountriesContext = createContext({} as CountriesContextType);
+
+type CountriesProviderProps = {
+    children: ReactNode
+}
+export const CountriesProvider = ({children} :CountriesProviderProps) => {
+    const cstate = useGlobalCountriesState();
+    return (
+        <CountriesContext.Provider value={cstate}>
+            {children}
+        </CountriesContext.Provider>
+    )
+}

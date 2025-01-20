8108ec7320e9f95fba8eddb27b3942831feac96c
Leandrho <leandro.coder01@gmail.com>
2025-01-20 17:43:44 -0300

    add selector component

---
+++ src/components/CountriesInputSelector.tsx
@@ -0,0 +1,54 @@
+import { useRef } from 'react';
+
+type CountriesRegionSelectorProps = {
+    regions : string[],
+    filterByRegion : (region:string)=>void
+}
+
+export const CountriesRegionSelector = ({filterByRegion, regions}:CountriesRegionSelectorProps) => {
+
+        const menuRef = useRef<HTMLDivElement>(null);
+        const butNameRef = useRef<HTMLParagraphElement>(null)
+        const handleMenuClose = () => {
+            if(menuRef.current){
+                if(menuRef.current.classList.contains('hidden')){
+                    menuRef.current.classList.remove('hidden');
+                    menuRef.current.classList.add('flex');
+                }
+                else {
+                    menuRef.current.classList.remove('flex');
+                    menuRef.current.classList.add('hidden');
+                }
+            }
+        }
+        const handleChange = (selected :string) => {
+            handleMenuClose();
+            if(butNameRef.current){
+                butNameRef.current.innerText=selected;
+            }
+            filterByRegion(selected);
+        }
+  return (
+            <div className="max-w-[480px] flex flex-col gap-2">
+                <button className="px-12 flex items-center justify-between rounded-xl shadow-md min-h-24 bg-white dark:bg-darkBlueDark cursor-pointer font-medium"
+                    onClick={()=> handleMenuClose()}
+                >
+                    <p ref={butNameRef} className="text-veryDarkBlueLight dark:text-veryDarkGrayLight">Filter by Region</p>
+                    <svg className="max-w-5 text-veryDarkBlueLight dark:text-veryDarkGrayLight" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="m12 14.586 6.293-6.293a1 1 0 1 1 1.414 1.414l-6.646 6.647a1.5 1.5 0 0 1-2.122 0L4.293 9.707a1 1 0 0 1 1.414-1.414L12 14.586z" fill="currentColor"></path></g></svg>
+                </button>
+                <div ref={menuRef} className={`hidden flex-col bg-white dark:bg-darkBlueDark shadow-md rounded-xl px-6 py-8`}>
+                    {
+                    regions.map((region)=>(
+                        <a key={region} href='#' 
+                            className="py-2 px-6 text-veryDarkBlueLight dark:text-veryDarkGrayLight font-medium cursor-pointer rounded-md hover:bg-veryDarkGrayLight hover:dark:bg-veryDarkBlueDark"
+                            onClick={()=>handleChange(region)}
+                        >
+                            {region}
+                        </a>
+                    ))
+                    
+                    }
+                </div>
+            </div>
+  )
+}

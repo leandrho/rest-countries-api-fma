a356512bccc406d0ed7ca442a30b6e3204bde31b
Leandrho <leandro.coder01@gmail.com>
2025-01-20 12:51:10 -0300

    ...

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
+import { CountriesProvider } from './contexts/CountriesContext';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <CountriesProvider>
+      <App />
+    </CountriesProvider>
   </StrictMode>,
 )

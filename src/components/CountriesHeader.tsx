6ad8121a75588dfc2742b6c3066e3f4f9deec884
Leandrho <leandro.coder01@gmail.com>
2025-01-21 15:47:08 -0300

    responsive header

--- src/components/CountriesHeader.tsx
+++ src/components/CountriesHeader.tsx
@@ -4,15 +4,15 @@ export const CountriesHeader = () => {
     const {toogleTheme} = useCountries();
   return (
     <header className="flex justify-between px-6 py-10 shadow-md bg-white dark:bg-darkBlueDark">
-        <h1 className="text-xl font-bold text-veryDarkBlueLight dark:text-white sm:text-xl">Where in the world?</h1>
-        <button className="flex items-center gap-4 "
+        <h1 className="text-lg sm:text-xl font-bold text-veryDarkBlueLight dark:text-white ">Where in the world?</h1>
+        <button className="flex items-center gap-2 sm:gap-4 "
                 onClick={()=>toogleTheme()}
         >
             <figure className="">
                  
-                <svg className={`text-veryDarkBlueLight dark:text-white w-7`} fill="#ffffff" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="currentColor" strokeWidth="1.6"> <title>moon</title> <path d="M10.895 7.574c0 7.55 5.179 13.67 11.567 13.67 1.588 0 3.101-0.38 4.479-1.063-1.695 4.46-5.996 7.636-11.051 7.636-6.533 0-11.83-5.297-11.83-11.83 0-4.82 2.888-8.959 7.023-10.803-0.116 0.778-0.188 1.573-0.188 2.39z"></path> </g><g id="SVGRepo_iconCarrier"> <title>moon</title> <path d="M10.895 7.574c0 7.55 5.179 13.67 11.567 13.67 1.588 0 3.101-0.38 4.479-1.063-1.695 4.46-5.996 7.636-11.051 7.636-6.533 0-11.83-5.297-11.83-11.83 0-4.82 2.888-8.959 7.023-10.803-0.116 0.778-0.188 1.573-0.188 2.39z"></path> </g></svg>
+                <svg className='text-veryDarkBlueLight dark:text-white w-7' fill="#ffffff" viewBox="0 0 32 32" version="1.1" xmlns="http://www.w3.org/2000/svg"><g id="SVGRepo_bgCarrier" strokeWidth="0"></g><g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round" stroke="currentColor" strokeWidth="1.6"> <title>moon</title> <path d="M10.895 7.574c0 7.55 5.179 13.67 11.567 13.67 1.588 0 3.101-0.38 4.479-1.063-1.695 4.46-5.996 7.636-11.051 7.636-6.533 0-11.83-5.297-11.83-11.83 0-4.82 2.888-8.959 7.023-10.803-0.116 0.778-0.188 1.573-0.188 2.39z"></path> </g><g id="SVGRepo_iconCarrier"> <title>moon</title> <path d="M10.895 7.574c0 7.55 5.179 13.67 11.567 13.67 1.588 0 3.101-0.38 4.479-1.063-1.695 4.46-5.996 7.636-11.051 7.636-6.533 0-11.83-5.297-11.83-11.83 0-4.82 2.888-8.959 7.023-10.803-0.116 0.778-0.188 1.573-0.188 2.39z"></path> </g></svg>
             </figure>
-            <p className="text-lg font-medium text-veryDarkBlueLight dark:text-white sm:text-base">Dark Mode</p>
+            <p className="text-base font-medium text-veryDarkBlueLight dark:text-white sm:text-lg">Dark Mode</p>
         </button>
     </header>
   )

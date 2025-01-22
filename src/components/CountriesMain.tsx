3c3bcd0b131b9b25162add8e135d28aa1a3e4c6d
Leandrho <leandro.coder01@gmail.com>
2025-01-22 11:27:56 -0300

    using loading and add reload button

--- src/components/CountriesMain.tsx
+++ src/components/CountriesMain.tsx
@@ -4,11 +4,12 @@ import { useEffect, useState } from 'react';
 import { CountryInfoList } from '../types';
 import { useCountries } from '../hooks/useCountries';
 import { CountriesCardContainer } from './CountriesCardContainer';
+import { Loading } from './Loading';
 
 const regions = ['All', 'Africa', 'Americas', 'Antarctic', 'Asia', 'Europe', 'Oceania'];
 
 export const CountriesMain = () => {
-    const { countries } = useCountries()
+    const { countries, reloadCountries, loading } = useCountries()
     const [countriesVisibles, setCountriesVisibles] = useState<CountryInfoList>(countries)
     const filterByRegion = (region :string):void =>{
         const r=region.toLowerCase();
@@ -33,14 +34,28 @@ export const CountriesMain = () => {
     }, [countries])
     
     return (
-        <main className='bg-veryDarkGrayLight dark:bg-veryDarkBlueDark flex justify-center flex-grow'>
-            <section className="xl:max-w-[1284px] flex-grow px-4 py-8 flex flex-col gap-16 ">
-                <div className='flex flex-col gap-8 xl:flex-row xl:justify-between'>
-                    <CountriesSearch matchCountries={searchCountry}/>
-                    <CountriesRegionSelector filterByRegion={filterByRegion} regions={regions}/>
-                </div>
-                <CountriesCardContainer countries={countriesVisibles}/>
-            </section>
+        <main className='flex justify-center flex-grow bg-veryDarkGrayLight dark:bg-veryDarkBlueDark'>
+            {   
+                loading? <Loading />
+                :<section className="xl:max-w-[1284px] flex-grow px-4 py-8 flex flex-col gap-16 ">
+                    <div className='flex flex-col gap-8 xl:flex-row xl:justify-between'>
+                        <CountriesSearch matchCountries={searchCountry}/>
+                        <CountriesRegionSelector filterByRegion={filterByRegion} regions={regions}/>
+                    </div>
+                    {
+                        countriesVisibles.length > 0 
+                        ?   <CountriesCardContainer countries={countriesVisibles}/>
+                        :   <section className="flex-grow flex items-center justify-center">
+                                <button className='px-8 py-4 self-center bg-white dark:bg-darkBlueDark text-veryDarkBlueLight dark:text-white rounded-md text-2xl max-w-36 shadow-md hover:bg-gray-200 hover:dark:bg-gray-500'
+                                    onClick={()=>reloadCountries()}
+                                >
+                                    Reload
+                                </button>
+                            </section>
+                    }
+                    
+                </section>
+            }
         </main>
         
     )

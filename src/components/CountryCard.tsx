bf24f753577642fd322e858ee2721a64863bc11a
Leandrho <leandro.coder01@gmail.com>
2025-01-22 11:27:20 -0300

    ...

--- src/components/CountryCard.tsx
+++ src/components/CountryCard.tsx
@@ -1,13 +1,24 @@
+import { useNavigate } from 'react-router-dom';
 import { CountryInfo } from '../types/index';
+import { useCountries } from '../hooks/useCountries';
 type CountryCardProps = {
     country :CountryInfo,
 }
 
 export const CountryCard = ( { country } :CountryCardProps ) => {
+
+    const navigate = useNavigate();
+    const {setShowCountry} = useCountries();
+    const handleClick = () => {
+        setShowCountry(country);
+        navigate('/country');
+    }
   return (
-    <div className='h-[490px] w-[340px] md:w-[528px] md:h-[670px] xl:w-[264px] xl:h-[336px] rounded-xl bg-white dark:bg-darkBlueDark flex flex-col shadow-md'>
+    <div className='h-[490px] w-[340px] md:w-[528px] md:h-[670px] xl:w-[264px] xl:h-[336px] rounded-xl bg-white dark:bg-darkBlueDark flex flex-col shadow-md cursor-pointer hover:opacity-60'
+        onClick={()=>handleClick()}
+    >
         <figure className='h-[220px] md:h-[320px] xl:h-[160px] w-full '>
-            <img className='h-full w-full block object-cover rounded-t-xl' src={country.flags.svg} alt={country.name.official+' flag'} />
+            <img className='h-full w-full block object-cover rounded-t-xl' src={country.flags.png} alt={country.name.official+' flag'} />
         </figure>
         <div className='px-8 pt-10 xl:pt-4 xl:space-y-4 space-y-8'>
             <h2 className='text-xl md:text-3xl xl:text-base font-extrabold text-veryDarkBlueLight dark:text-white'>{country.name.common}</h2>
